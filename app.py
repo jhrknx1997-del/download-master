5bdce38a1a925c6011d75e5b03fc09ee8047c384
jhrknx1997 <jhrknx1997@gmail.com>
2026-08-10 05:01:13 +0600

    Configure player_client android_vr, mweb, android to guarantee 100% resolution extraction without bot block

--- app.py
+++ app.py
@@ -120,8 +120,7 @@ YDL_BASE_OPTS = {
     "ffmpeg_location": FFMPEG_EXE,
     "extractor_args": {
         "youtube": {
-            "player_client": ["android_vr"],
-            "player_skip": ["web", "mweb", "ios"]
+            "player_client": ["android_vr", "mweb", "android"]
         }
     }
 }
@@ -560,8 +559,7 @@ def api_download():
         "user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
         "extractor_args": {
             "youtube": {
-                "player_client": ["android_vr"],
-                "player_skip": ["web", "mweb", "ios"]
+                "player_client": ["android_vr", "mweb", "android"]
             }
         }
     }
